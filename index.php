c643642536cd0bb86549198a4f79bc537728c47d
Masdi <masdi.msd@gmail.com>
2017-06-16 07:57:40 +0700

    update finished

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script> -->
 <script type="text/javascript" src="assets/datatables/datatables.min.js"></script>
 <script type="text/javascript">
-  window.alert = function(){};
+  // window.alert = function(){};
   var defaultCSS = document.getElementById('bootstrap-css');
   function changeCSS(css){
     if(css) $('head > link').filter(':first').replaceWith('<link rel="stylesheet" href="'+ css +'" type="text/css" />'); 
@@ -47,7 +47,7 @@
               <th>Delete</th>
             </thead>
             <tbody>
-              <tr>
+              <!-- <tr>
                 <td><input type="checkbox" class="checkthis" /></td>
                 <td>Mohsin</td>
                 <td>Irshad</td>
@@ -61,7 +61,7 @@
                 <td>
 
                 <p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
-              </tr>
+              </tr> -->
             </tbody>
           </table>
         </div>
@@ -71,25 +71,38 @@
   <div class="modal fade" id="edit" tabindex="-1" role="dialog" aria-labelledby="edit" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
+      <form id="membersForm" action="operations.php?type=save">
         <div class="modal-header">
+        <input type="hidden" name="_id">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
           <h4 class="modal-title custom_align" id="Heading">Edit Your Detail</h4>
         </div>
         <div class="modal-body">
+
           <div class="form-group">
-            <input class="form-control " type="text" placeholder="Mohsin">
+            <input class="form-control " type="text" placeholder="[first_name]" name="first_name">
           </div>
           <div class="form-group">
 
-            <input class="form-control " type="text" placeholder="Irshad">
+            <input class="form-control " type="text" placeholder="[last_name]" name="last_name">
           </div>
           <div class="form-group">
-            <textarea rows="2" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
+
+            <input class="form-control " type="text" placeholder="[email]" name="email">
+          </div>
+
+          <div class="form-group">
+
+            <input class="form-control " type="text" placeholder="[contact]" name="contact">
+          </div>
+          <div class="form-group">
+            <textarea rows="2" name="address" class="form-control" placeholder="CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan"></textarea>
           </div>
         </div>
         <div class="modal-footer ">
-          <button type="button" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
+          <button id="btnUpdate" type="submit" class="btn btn-warning btn-lg" style="width: 100%;"><span class="glyphicon glyphicon-ok-sign"></span> Update</button>
         </div>
+        </form>
       </div>
       <!-- /.modal-content --> 
     </div>
@@ -108,7 +121,7 @@
 
         </div>
         <div class="modal-footer ">
-          <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
+          <button type="button" data-id="" id="confirmDelete" class="btn btn-success" ><span class="glyphicon glyphicon-ok-sign"></span> Yes</button>
           <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> No</button>
         </div>
       </div>
@@ -118,7 +131,36 @@
   </div>
   <script type="text/javascript">
     $(document).ready(function(){
-      $('#mytable').DataTable({
+      $.deleteDialog=function(el){
+        $('#confirmDelete').removeAttr('disabled');
+        $('#confirmDelete').html('<span class="glyphicon glyphicon-ok-sign"></span>Yes');
+        var rowData =  $(el).closest('tr');
+        rowData=myTable.row(rowData).data();
+        $('#confirmDelete').data('id',rowData._id);
+        $('#delete').modal();
+      }
+      $.editDialog = function(el){
+        var rowData =  $(el).closest('tr');
+        rowData=myTable.row(rowData).data();
+        $('#edit input[name=first_name]').attr('placeholder',rowData.first_name);
+        $('#edit input[name=last_name]').attr('placeholder',rowData.last_name);
+        $('#edit input[name=email]').attr('placeholder',rowData.email);
+        $('#edit input[name=contact]').attr('placeholder',rowData.contact);
+        $('#edit textarea[name=address]').attr('placeholder',rowData.address);
+        
+        $('#edit input[name=first_name]').val(rowData.first_name);
+        $('#edit input[name=last_name]').val(rowData.last_name);
+        $('#edit input[name=email]').val(rowData.email);
+        $('#edit input[name=contact]').val(rowData.contact);
+        $('#edit textarea[name=address]').html(rowData.address);
+        $('#edit input[name=_id]').val(rowData._id);
+
+        $('#btnUpdate').removeAttr('disabled');
+        $('#btnUpdate').html('<span class="glyphicon glyphicon-ok-sign"></span>Update'); 
+        $('#edit').modal();
+        
+      };
+      var myTable=$('#mytable').DataTable({
         serverSide:true,
         processing:true,
         ajax:{
@@ -137,21 +179,18 @@
           {data:"_id",render:function(data) {
 
             return '<p data-id="'+data+'" data-placement="top" data-toggle="tooltip" title="Edit">'
-              +'<button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="x_modal" data-target="#edit" >'
+              +'<button class="btn btn-primary btn-xs" data-title="Edit" onClick="$.editDialog(this);" data-target="#edit" >'
               +'<span class="glyphicon glyphicon-pencil"></span>'
               +'</button></p>';
           }},
           {data:"_id",render:function(data){
               return '<p data-id="'+data+'"data-placement="top" data-toggle="tooltip" title="Delete">'
-              +'<button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" >'
+              +'<button class="btn btn-danger btn-xs" data-title="Delete" onClick="$.deleteDialog(this);" data-target="#delete" >'
               +'<span class="glyphicon glyphicon-trash"></span></button></p>';
           }},
         ]
       });
 
-      $('body').on('click', 'table button[data-toggle="x_modal"]', function() {
-        alert($(this).data('title'));
-      });
       $("#mytable #checkall").click(function () {
         if ($("#mytable #checkall").is(':checked')) {
           $("#mytable input[type=checkbox]").each(function () {
@@ -166,6 +205,35 @@
       });
 
       $("[data-toggle=tooltip]").tooltip();
+      $('#confirmDelete').click(function(ev){
+        $(this).attr('disabled','disabled');
+        
+        $('#confirmDelete').html('<span class="glyphicon glyphicon-ok-sign"></span>Please Wait');
+        // $('#delete').modal('toggle');
+      });
+      $('#membersForm').submit(function(e){
+        var form=$(this);
+        var data=form.serialize();
+        $('#btnUpdate').attr('disabled','disabled'); 
+        $('#btnUpdate').html('<span class="glyphicon glyphicon-ok-sign"></span>Please Wait'); 
+        $.ajax({
+          url:form.attr('action'),
+          type:'POST',
+          dataType:'JSON',
+          data:data,
+          success:function(response){
+            console.log(response);
+            if(response.status=='failed'){
+              alert(response.message);
+            }
+            $('#edit').modal('toggle');
+            myTable.ajax.reload();
+          },
+
+        })
+        e.preventDefault();
+        return false;
+      });
 
     });
 
--- operations.php
+++ operations.php
@@ -4,22 +4,26 @@ require 'vendor/autoload.php';
 $config=require 'config.php';
 
 $curl = new \Curl\Curl();
-$member_id=empty($_GET['id'])?'':$_GET['id'];
+$member_id=empty($_POST['_id'])?'':$_POST['_id'];
 $result=['status'=>'failed','message'=>'unknown operation type'];
-switch($_GET['type']){
+$type=$_GET['type'];
+if('save'==$type){
+	$type=empty($_POST['_id'])?'create':'update';
+}
+switch($type){
 	case 'update':
 
-		$result=$curl->patch($config['baseApi'].'members/'.$member_id);
+		$result=$curl->patch($config['baseApi'].'members/'.$member_id,$_POST);
 		break;
 		
 	case 'edit':
 
-		$result=$curl->get($config['baseApi'].'members/'.$member_id);
+		$result=$curl->get($config['baseApi'].'members/'.$member_id,$_POST);
 		break;
 		
 	case 'delete':
 
-		$result=$curl->delete($config['baseApi'].'members/'.$member_id);
+		$result=$curl->delete($config['baseApi'].'members/'.$member_id,$_POST);
 		break;
 		
 	case 'create':
